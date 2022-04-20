592d9beb0cff4803f4e1e92e64b49c68074890e2
hkuhnau <95493237+hkuhnau@users.noreply.github.com>
2022-04-20 11:37:22 -0500

    Button logic for mobile view
    when the screen is in mobile view, and the patty is clicked, the list-items get a bootstrap button class applied

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 // the patty icon has no onclick attribute...the event listener runs the function
 const patty = document.querySelector(".patty");
 const myList = document.querySelector(".navigation");
+const listItem = document.querySelectorAll(".list-items");
+
 
 patty.addEventListener('click', () => {
     const visible = myList.getAttribute("data-visible");
     if (visible === "false") {
         myList.setAttribute("data-visible", true);
+        //switching the list items to have the button class when in mobile view and visible
+        //the list items are contained in a node list
+        for (i = 0; i < listItem.length; i ++) {
+            //looping over each one and setting their boostrap class
+            listItem[i].setAttribute("class", "btn btn-outline-info");
+        }
     } else if (visible === "true") {
         myList.setAttribute("data-visible", false);
+        for (i = 0; i < listItem.length; i ++) {
+            //looping over each one and setting their boostrap class
+            listItem[i].setAttribute("class", "list-items");
+        }
     }
 });
