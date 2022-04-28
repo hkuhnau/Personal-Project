f5f6ba68bd043e18576154d6946efd842c407ae0
hkuhnau <95493237+hkuhnau@users.noreply.github.com>
2022-04-28 15:12:56 -0500

    resized the nav bar in mobile view
    made the navigation bar smaller in mobile view, and allowing it to grow when the patty is clicked via JS

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 const patty = document.querySelector(".patty");
 const myList = document.querySelector(".navigation");
 const listItem = document.querySelectorAll(".list-items");
-
+const nav = document.querySelector("nav");
 
 patty.addEventListener('click', () => {
     const visible = myList.getAttribute("data-visible");
     if (visible === "false") {
         myList.setAttribute("data-visible", true);
+        nav.setAttribute("data-visible", true);
         //switching the list items to have the button class when in mobile view and visible
         //the list items are contained in a node list
         for (i = 0; i < listItem.length; i ++) {
@@ -16,6 +17,7 @@ patty.addEventListener('click', () => {
         }
     } else if (visible === "true") {
         myList.setAttribute("data-visible", false);
+        nav.setAttribute("data-visible", false);
         for (i = 0; i < listItem.length; i ++) {
             //looping over each one and setting their boostrap class
             listItem[i].setAttribute("class", "list-items");
