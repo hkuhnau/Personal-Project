49d9e60a712d1e66ba5e8bb718efaeff004a7192
hkuhnau <95493237+hkuhnau@users.noreply.github.com>
2022-06-08 09:13:51 -0500

    added timeout functions
    changes the label back to its original words "check your answer"

--- javascript.js
+++ javascript.js
@@ -9,22 +9,47 @@ const getTextOne = () => {
 
     if (theResult == false) {
         labelOne.style.color = "red";
+        labelOne.innerHTML = "Incorrect, Try Again";
+        const myTimeout = setTimeout(originalLabel, 2200);
+        function originalLabel() {
+            labelOne.style.color = "white";
+            labelOne.innerHTML = "Check your Answer";
+        }
     } else {
         labelOne.style.color = "green";
+        labelOne.innerHTML = "Correct!";
+        const myTimeout = setTimeout(originalLabel, 2200);
+        function originalLabel() {
+            labelOne.style.color = "white";
+            labelOne.innerHTML = "Check your Answer";
+        }
     }
 
+    // return labelOne.innerHTML = "Check your Answer";
 }
 
 const getTextTwo = () => {
     let textTwo = document.getElementById("textTwo").value;
     let labelTwo = document.getElementById("labelTwo");
-    let inputOne = /^myAge\s=\s+[0-9]+\;/;
+    let inputOne = /^let\smyAge\s=\s+[0-9]+\;/;
     let theResult = inputOne.test(textTwo);
     console.log(textTwo);
     if (theResult == false) {
         labelTwo.style.color = "red";
+        labelTwo.innerHTML = "Incorrect, Try Again";
+        const myTimeout = setTimeout(originalLabel, 2200);
+        function originalLabel() {
+            labelTwo.style.color = "white";
+            labelTwo.innerHTML = "Check your Answer";
+        }
     } else {
         labelTwo.style.color = "green";
+        labelTwo.innerHTML = "Correct!";
+        const myTimeout = setTimeout(originalLabel, 2200);
+        function originalLabel() {
+            labelTwo.style.color = "white";
+            labelTwo.innerHTML = "Check your Answer";
+        }
     }
 
 }
