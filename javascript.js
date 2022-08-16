13ef30ec2b25918e319706c5a91b0e05ef37bfa3
hkuhnau <95493237+hkuhnau@users.noreply.github.com>
2022-08-16 11:30:16 -0500

    added the functionality to svg2

--- javascript.js
+++ javascript.js
@@ -53,23 +53,13 @@ const getTextTwo = () => {
     }
 
 }
-// const helpOne = () => {
-//     //define the helpsection
-//     const helpSection = document.getElementById("helpSection");
-//     //create the text node and append to the element
-//     let node = document.createTextNode("Have you checked your spelling?");
-//     helpSection.appendChild(node);
-//     document.body.appendChild(helpSection);
-//     //applying styles to the section
-//     helpSection.style.color = "white";
-// }
 
 //define the svg
 const svg = document.getElementById("svg1");
 //define the helpsection
 const helpSection = document.getElementById("helpSection");
 //define the array of answers for the helpSection
-const array = ["Have you checked your spelling?", "Did you forget the semicolon?", "Do you have quotations around your name?"];
+const array = ["Hint: Have you checked your spelling?", "Hint: Did you forget the semicolon?", "Hint: Do you have quotations around your name?"];
 //loop over the array every time the button is clicked and add to the helpSection
 let i = 0;
 
@@ -81,7 +71,31 @@ function oneByOne(e) {
     }
     return [helpSection.innerHTML = array[i++],setTimeout(() => {
         helpSection.innerHTML = "";
-    }, 2000)]; 
+    }, 2000)];
   }
 }
 svg.onclick = oneByOne;
+
+
+
+//define the svg2
+const svg2 = document.getElementById("svg2");
+//define the helpsection
+const helpSection2 = document.getElementById("helpSection2");
+//define the array of answers for the helpSection
+const array2 = ["Hint: Have you checked your spelling?", "Hint: Did you forget the semicolon?", "Hint: The words are case sensitive"];
+//loop over the array every time the button is clicked and add to the helpSection
+let i2 = 0;
+
+function oneByOne2(e) {
+  for (i2; i2 <= 3; i2++) {
+    console.log(i2);
+    if (i2 == 3) {
+        i2 = 0;
+    }
+    return [helpSection2.innerHTML = array2[i2++],setTimeout(() => {
+        helpSection2.innerHTML = "";
+    }, 2000)];
+  }
+}
+svg2.onclick = oneByOne2;
